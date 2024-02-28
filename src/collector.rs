df1351e6affdf715084f86e8b26104bf54c495a0
Chad Collins <chadrcollins91@gmail.com>
2024-02-28 17:26:09 -0500

    Implemented new logic for annotation collecting

--- src/collector.rs
+++ src/collector.rs
@@ -100,6 +100,8 @@ struct CollectionData<'a> {
     nest_level: usize,
     count: usize,
     ended: bool,
+    current_part: usize,
+    current_part_tokens: Vec<LexerToken>,
 }
 
 impl<'a> CollectionData<'a> {
@@ -110,6 +112,8 @@ impl<'a> CollectionData<'a> {
             nest_level,
             count: 0,
             ended: false,
+            current_part: 0,
+            current_part_tokens: vec![],
         }
     }
 }
@@ -180,65 +184,138 @@ impl Collector {
                     nest_level,
                     count,
                     ended,
+                    current_part,
+                    current_part_tokens,
                 }) => {
-                    if !sink
-                        .ignore_for_end_condition_list
-                        .contains(&token.get_token_type())
-                    {
-                        *count += 1;
-                    }
+                    match sink.part_parsers.get(*current_part) {
+                        None => {}
+                        Some(parser) => {
+                            let part_ended = match parser.behavior {
+                                PartBehavior::UntilNewline => token.get_text().contains("\n"),
+                                _ => unimplemented!(),
+                            };
 
-                    *ended = match &sink.end_condition {
-                        EndCondition::Lone => unreachable!(), // never added to stack
-                        EndCondition::Count(desired_count) => count == desired_count, // info.non_ignored_token_count >= count,
-                        EndCondition::UntilToken(token_type) => {
-                            token_type == &token.get_token_type()
-                                && nest_level == &current_nest_level
-                        }
-                        EndCondition::UntilNewline => token.get_text().contains("\n"),
-                        EndCondition::UntilAnnotation(annotation) => {
-                            token.get_token_type() == TokenType::Annotation
-                                && token.get_text() == annotation
-                        }
-                    };
-
-                    // Don't add nested annotations to tokens if we have a sink for it
-                    match token.get_token_type() {
-                        TokenType::Annotation => match self
-                            .sinks
-                            .iter()
-                            .find(|item| &item.annotation_text == token.get_text())
-                        {
-                            // No sink for annotation, add to tokens
-                            None => block.tokens.push(token.clone()),
-                            Some(sink) => match sink.end_condition {
-                                EndCondition::Lone => blocks
-                                    .push(TokenBlock::with_annotation(token.get_text().clone())),
+                            // Don't add nested annotations to tokens if we have a sink for it
+                            let nested_sink = match token.get_token_type() {
+                                TokenType::Annotation => match self
+                                    .sinks
+                                    .iter()
+                                    .find(|item| &item.annotation_text == token.get_text())
+                                {
+                                    // No sink for annotation, add to tokens
+                                    None => {
+                                        current_part_tokens.push(token.clone());
+                                        None
+                                    }
+                                    Some(sink) => match sink.end_condition {
+                                        EndCondition::Lone => {
+                                            blocks.push(TokenBlock::with_annotation(
+                                                token.get_text().clone(),
+                                            ));
+                                            None
+                                        }
+                                        _ => Some(sink),
+                                    },
+                                },
                                 _ => {
+                                    current_part_tokens.push(token.clone());
+                                    None
+                                }
+                            };
+
+                            if part_ended {
+                                block.parts.push(current_part_tokens.clone());
+                                *current_part_tokens = vec![];
+                                *current_part = *current_part + 1;
+                            }
+
+                            *ended = *current_part >= sink.part_parsers.len();
+
+                            match nested_sink {
+                                None => (),
+                                Some(sink) => {
                                     annotations_stack.push(CollectionData::new(
                                         sink,
                                         TokenBlock::with_annotation(token.get_text().clone()),
                                         current_nest_level,
                                     ));
                                 }
-                            },
-                        },
-                        _ => block.tokens.push(token.clone()),
-                    }
+                            }
 
-                    // Possible to have multiple ended blocks in stack
-                    // loop until all have been popped
-                    while annotations_stack
-                        .last()
-                        .and_then(|b| Some(b.ended))
-                        .unwrap_or(false)
-                    {
-                        let data = annotations_stack.pop().unwrap(); // has to exist to get to this branch
-                        match annotations_stack.last_mut() {
-                            None => blocks.push(data.block),
-                            Some(parent) => parent.block.nested.push(data.block),
+                            // Possible to have multiple ended blocks in stack
+                            // loop until all have been popped
+                            while annotations_stack
+                                .last()
+                                .and_then(|b| Some(b.ended))
+                                .unwrap_or(false)
+                            {
+                                let data = annotations_stack.pop().unwrap(); // has to exist to get to this branch
+                                match annotations_stack.last_mut() {
+                                    None => blocks.push(data.block),
+                                    Some(parent) => parent.block.nested.push(data.block),
+                                }
+                            }
                         }
                     }
+
+                    // if !sink
+                    //     .ignore_for_end_condition_list
+                    //     .contains(&token.get_token_type())
+                    // {
+                    //     *count += 1;
+                    // }
+                    //
+                    // *ended = match &sink.end_condition {
+                    //     EndCondition::Lone => unreachable!(), // never added to stack
+                    //     EndCondition::Count(desired_count) => count == desired_count, // info.non_ignored_token_count >= count,
+                    //     EndCondition::UntilToken(token_type) => {
+                    //         token_type == &token.get_token_type()
+                    //             && nest_level == &current_nest_level
+                    //     }
+                    //     EndCondition::UntilNewline => token.get_text().contains("\n"),
+                    //     EndCondition::UntilAnnotation(annotation) => {
+                    //         token.get_token_type() == TokenType::Annotation
+                    //             && token.get_text() == annotation
+                    //     }
+                    // };
+                    //
+                    // // Don't add nested annotations to tokens if we have a sink for it
+                    // match token.get_token_type() {
+                    //     TokenType::Annotation => match self
+                    //         .sinks
+                    //         .iter()
+                    //         .find(|item| &item.annotation_text == token.get_text())
+                    //     {
+                    //         // No sink for annotation, add to tokens
+                    //         None => block.tokens.push(token.clone()),
+                    //         Some(sink) => match sink.end_condition {
+                    //             EndCondition::Lone => blocks
+                    //                 .push(TokenBlock::with_annotation(token.get_text().clone())),
+                    //             _ => {
+                    //                 annotations_stack.push(CollectionData::new(
+                    //                     sink,
+                    //                     TokenBlock::with_annotation(token.get_text().clone()),
+                    //                     current_nest_level,
+                    //                 ));
+                    //             }
+                    //         },
+                    //     },
+                    //     _ => block.tokens.push(token.clone()),
+                    // }
+                    //
+                    // // Possible to have multiple ended blocks in stack
+                    // // loop until all have been popped
+                    // while annotations_stack
+                    //     .last()
+                    //     .and_then(|b| Some(b.ended))
+                    //     .unwrap_or(false)
+                    // {
+                    //     let data = annotations_stack.pop().unwrap(); // has to exist to get to this branch
+                    //     match annotations_stack.last_mut() {
+                    //         None => blocks.push(data.block),
+                    //         Some(parent) => parent.block.nested.push(data.block),
+                    //     }
+                    // }
                 }
             }
         }
@@ -371,15 +448,7 @@ mod collecting {
             vec![
                 TokenBlock::new_with_parts(
                     "@Test".to_string(),
-                    vec![
-                        LexerToken::new(" ".to_string(), TokenType::Whitespace, 0, 5),
-                        LexerToken::new("5".to_string(), TokenType::Number, 0, 6),
-                        LexerToken::new(" ".to_string(), TokenType::Whitespace, 0, 7),
-                        LexerToken::new("+".to_string(), TokenType::PlusSign, 0, 8),
-                        LexerToken::new(" ".to_string(), TokenType::Whitespace, 0, 9),
-                        LexerToken::new("5".to_string(), TokenType::Number, 0, 10),
-                        LexerToken::new("   \n   ".to_string(), TokenType::Whitespace, 0, 11),
-                    ],
+                    vec![],
                     vec![vec![
                         LexerToken::new(" ".to_string(), TokenType::Whitespace, 0, 5),
                         LexerToken::new("5".to_string(), TokenType::Number, 0, 6),
@@ -390,13 +459,17 @@ mod collecting {
                         LexerToken::new("   \n   ".to_string(), TokenType::Whitespace, 0, 11),
                     ]]
                 ),
-                TokenBlock::with_tokens(vec![
-                    LexerToken::new("5".to_string(), TokenType::Number, 1, 3),
-                    LexerToken::new(" ".to_string(), TokenType::Whitespace, 1, 4),
-                    LexerToken::new("+".to_string(), TokenType::PlusSign, 1, 5),
-                    LexerToken::new(" ".to_string(), TokenType::Whitespace, 1, 6),
-                    LexerToken::new("5".to_string(), TokenType::Number, 1, 7),
-                ])
+                TokenBlock::new_with_parts(
+                    "".to_string(),
+                    vec![
+                        LexerToken::new("5".to_string(), TokenType::Number, 1, 3),
+                        LexerToken::new(" ".to_string(), TokenType::Whitespace, 1, 4),
+                        LexerToken::new("+".to_string(), TokenType::PlusSign, 1, 5),
+                        LexerToken::new(" ".to_string(), TokenType::Whitespace, 1, 6),
+                        LexerToken::new("5".to_string(), TokenType::Number, 1, 7),
+                    ],
+                    vec![]
+                )
             ]
         );
     }
